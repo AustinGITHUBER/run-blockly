3fdd35230f6a983984ca6a357e82d06b56005eaa
AustinGITHUBER <73083603+AustinGITHUBER@users.noreply.github.com>
2023-02-08 14:24:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 'use strict'
 document.querySelector('div').remove()
-let div = document.createElement('div')
+window['div'] = document.createElement('div')
 div.style.position = 'absolute'
 div.style.top = '0px'
 div.style.left = '0px'
 div.style.width = `${1e308}px`
-div.style.height = `4991px`
+div.style.height = `1089px`
 document.body.append(div)
-window['workspace'] = Blockly.inject(div, {
-    "toolbox": {
-        "kind": "flyoutToolbox",
-        "contents": Object.keys(Blockly.Blocks).map(type => ({
+window['toolbox'] = {
+    "kind": "categoryToolbox",
+    "contents": Object.entries(Blockly.libraryBlocks).slice(0, 9).map(([name, {blocks}]) => ({
+        "kind": "category",
+        name,
+        "contents": Object.keys(blocks).map(type => ({
             "kind": "block",
             type
         }))
-    }
-})
-let button = document.createElement('button')
+    }))
+}
+window['workspace'] = Blockly.inject(div, {toolbox})
+window['toolboxObject'] = workspace.getToolbox()
+window['button'] = document.createElement('button')
 button.style.position = 'fixed'
 button.style.bottom = '0px'
 button.style.right = '0px'
